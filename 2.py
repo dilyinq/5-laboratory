330f788c22584e358077965f31fc8a2591a76ae5
1 <diany.kor.2019@mail.ru>
2024-03-23 23:44:41 +0300

    /solutions

--- 2.py
+++ 2.py
@@ -1,11 +1,9 @@
-def zud (list):
-    unique_elems = []
-    for elem in list:
-        if elem in unique_elems:
-            print(f"Повторяющийся элемент: {elem}")
-            break
-        unique_elems.append(elem)
-    else:
-        print("Повторяющиеся элементы отсутствуют")
-my_list = [21, 7, 3, 9, 5, 3, 9, 7, 1]
-zud (my_list)
+my_list = [21, 2, 1, 21, 8, 5, 9, 1]
+duplicates = []
+for item in my_list:
+    if my_list.count(item) > 1 and item not in duplicates:
+        duplicates.append(item)
+if duplicates:
+    print("Повторяющиеся элементы в списке:", duplicates)
+else:
+    print("В списке нет повторяющихся элементов.")
